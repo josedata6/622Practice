bc69cbdfe59784fe7043a644f246d0fe00bcc347
josedata6 <jose.diaz@csun.edu>
2025-02-20 21:02:27 -0800

    loop testing

---
+++ loop-practice.py
@@ -0,0 +1,22 @@
+## class loop practice
+
+stopValue = int(input("Provide the stop value: "))
+startValue = int(input("Provide the start value: "))
+stepValue = int(input("Provide the step value: "))
+
+## startNum = int(input("Provide the starting number: "))
+
+
+
+if startValue < 0:
+    startValue = abs(starttNum)
+
+if stopValue  < 0:
+    iterationNum = abs(stopValue)
+
+product = 1
+iterator = 1
+
+for iterator in range(startValue, stopValue, stepValue):
+    product = product * startValue * (iterator +1)
+    print(product)
