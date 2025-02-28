a69b7059cd6b88ee576a34003405a5cfa079eb58
josedata6 <jose.diaz@csun.edu>
2025-02-27 21:24:18 -0800

    loop testing

--- for-while-loop.py
+++ for-while-loop.py
@@ -48,17 +48,35 @@
 #############
 ###### using while only adding even numbers
 
-number = input("give me a number: ")
-number = int(number)
-if number <0:
-    number = abs(number)
+# number = input("give me a number: ")
+# number = int(number)
+# if number <0:
+#     number = abs(number)
     
-i = 2
-total =0
+# i = 2
+# total =0
 
-while i<= number:
-    i+=2
-    total += i
-    
+# while i<= number:
+#     i+=2
+#     total += i
 
-print(total)
+# print(total)
+
+###########################################
+
+count = 0
+total = 0
+
+while True:
+    user_input= input("entera numer or type end to finish: ")
+    if user_input.lower()=="end": #making sure the input is lowercase
+        print("Thanks for your input")
+        break
+    elif user_input.isnumeric():
+        count+=1
+        total = total + float(user_input)
+    else: ### adding a check if inout is something else than end
+        print("your input is not valid")
+        pass
+
+    print(count, " ", total)
