43a1335f4f503ceab52a1f3b5e867412b636c567
josedata6 <jose.diaz@csun.edu>
2025-02-27 19:54:22 -0800

    Create while-loop.py

---
+++ while-loop.py
@@ -0,0 +1,12 @@
+#### while loop
+## 1. Ask user to provide number
+## 2. check if the input is not void
+## 3. if its void go to the end
+## 4. if it is not void add the input to the value of the sum
+
+theSum = 0 
+
+data = int(input("Enter number: "))
+
+while data != "":
+    sum + = data
